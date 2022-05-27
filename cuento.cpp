ff69af4fffa95f8e1bb820d5ece484f6ab82e076
lxxndrx07 <88805388+lxxndrx07@users.noreply.github.com>
2022-05-27 17:03:45 +0000

    Complete atributos y metodos de la clase Cuento

---
+++ cuento.cpp
@@ -0,0 +1,8 @@
+#include "cuento.h"
+
+
+  //Constructor
+  cuento::cuento(string titulo);
+
+  //Mostrar titulo
+  cuento::string mostrar_titulo();
---
+++ cuento.h
@@ -0,0 +1,25 @@
+#ifndef CUENTO_H
+#define CUENTO_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Cuento {
+
+private:
+  string titulo;
+
+public:
+  //Constructor
+  Cuento(string titulo);
+
+  //Mostrar titulo
+  string mostrar_titulo();
+
+  //Destructor
+  ~ Cuento();
+}
+
+#endif
