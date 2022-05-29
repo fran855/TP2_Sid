4203b8f32113086ccacda005f816dacf2ed13917
fran <fspaltro@fi.uba.ar>
2022-05-29 16:12:39 -0300

    Update

--- parser_escritores.cpp
+++ parser_escritores.cpp
@@ -1,24 +1,30 @@
 #include "parser_escritores.h"
 #include "escritor.h"
+#include <iostream>
+
+using namespace std;
 
 Parser_escritores::Parser_escritores(char ** argv){
     archivo.open(argv[1]);
 }
 
-Lista<Escritor> Parser_escritores::parsear(){
-    Lista<Escritor> lista_escritores;
-    
-    string auxiliar;
-
-    int i = 0;
+void Parser_escritores::generar_anonimo(){
     id = 0;
     nombre_apellido = "ANONIMO";
     nacionalidad = "¿?";
     anio_nacimiento = -1;
     anio_fallecimiento = -1;
+}
 
+Lista<Escritor> Parser_escritores::parsear(){
+    Lista<Escritor> lista_escritores;
+    
+    generar_anonimo();
     Escritor nuevo_escritor(id, nombre_apellido, nacionalidad, anio_nacimiento, anio_fallecimiento);
-    lista_escritores.alta(nuevo_escritor, i++);
+    lista_escritores.alta(nuevo_escritor, 0);
+    
+    string auxiliar;
+    int i = 1;
 
     while(!archivo.eof()){
         archivo >> id;
@@ -44,7 +50,9 @@ Lista<Escritor> Parser_escritores::parsear(){
         Escritor escritor(id, nombre_apellido, nacionalidad, anio_nacimiento, anio_fallecimiento);
         lista_escritores.alta(escritor, i);
     }
-    
-    archivo.close();
     return lista_escritores;
+}
+
+Parser_escritores::~Parser_escritores(){
+    archivo.close();
 }
