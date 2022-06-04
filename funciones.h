7c0b1140705c84d0f38f92edd877778cd9200b00
fran855 <65423343+fran855@users.noreply.github.com>
2022-06-03 23:02:09 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+//PRE: a y b cadenas válidas
+//POS: devuelve true si son iguales o false si son diferentes (sin case sensitive)
 bool son_iguales(const string a, const string b);
 
 #endif
